8ec5ac911ce8e66a6960393a0a95acf5fb5e9eb8
BFFV <bffarias@uc.cl>
2020-12-08 07:42:39 -0300

    :sparkles: Added graph states.

--- Examen/scripts/videogames.js
+++ Examen/scripts/videogames.js
@@ -4,7 +4,59 @@ const networkSize = {
   width: network.style('width'),
   height: Math.floor(parseInt(network.style('width'), 10) / 2)};
 network.style('height', `${networkSize.height}px`);
+const networkState = {level: 0, genre: '', year: '', tag: ''};
+let games = [];
+let currentGames = [];
+let tree = {};
 const selectedData = [];
+
+// Updates the network graph levels
+function updateTree() {
+  for (let genre in tree) {
+    tree[genre].count = 0;
+    for (let year in tree[genre].dates) {
+      tree[genre].dates[year].count = 0;
+      for (let tag in tree[genre].dates[year].tags) {
+        tree[genre].dates[year].tags[tag] = 0;
+      }
+    }
+  }
+  for (let game of currentGames) {
+    for (let genre of game.genres.split(';')) {
+      tree[genre].count ++;
+      const gameYear = game.release_date.slice(0, 4);
+      tree[genre].dates[gameYear].count ++;
+      for (let tag of game.steamspy_tags.split(';')) {
+        tree[genre].dates[gameYear].tags[tag] ++;
+      }
+    }
+  }
+}
+
+// Get current data for network graph
+function getLevelData() {
+  const {level, genre, year, tag} = networkState;
+  let levelData = [];
+  if (!level) {
+    for (let g in tree) {
+      levelData.push({name: g, count: tree[g].count});
+    }
+  } else if (level == 1) {
+    for (let d in tree[genre].dates) {
+      levelData.push({name: d, count: tree[genre].dates[d].count});
+    }
+  } else if (level == 2) {
+    for (let t in tree[genre].dates[year].tags) {
+      levelData.push({name: t, count: tree[genre].dates[year].tags[t]});
+    }
+  } else {
+    levelData = currentGames.filter((game) => {
+      return (game.genres.split(';').includes(genre)) &&
+       (game.release_date.slice(0, 4) == year) &&
+        (game.steamspy_tags.split(';').includes(tag))});
+  }
+  return levelData;
+}
 
 // Create/Update Bar Graph
 function updateGraph(selectedData, censusData) {
@@ -144,7 +196,7 @@ function updateGraph(selectedData, censusData) {
 }
 
 // Generate Steam Games Network
-const gameNetwork = (height, width, games) => {
+const gameNetwork = (height, width) => {
   // Graph Container
   const svg = network
     .append('svg')
@@ -250,11 +302,11 @@ const gameNetwork = (height, width, games) => {
 
   // Map DataJoin
   graph
-    .selectAll('path')
-    .data(games, (d) => d)
+    .selectAll('circle')
+    .data(getLevelData(), (d) => d.name)
     .join('circle')
       .attr('r', 30)
-      .attr('cx', (d) => parseInt(d.steam_appid) / 1000)
+      .attr('cx', 10)
       .attr('cy', 0)
       .attr('fill', (d) => 'red')
       .attr('stroke', '#1F1F1F')
@@ -262,6 +314,143 @@ const gameNetwork = (height, width, games) => {
     //.on('click', (_, d) => selected(d.properties.id))
     //.on('mouseenter', (_, d) => highlight(d.properties.id))
     //.on('mouseleave', (_, d) => stopHighlight(d.properties.id))
+}
+
+// Create/Update Bar Graph
+function updateNetwork(selectedData, censusData) {
+  // Census data
+  const data = censusData
+    .filter((c) => selectedData.includes(c.ID))
+    .sort((a, b) => selectedData.indexOf(a.ID) - selectedData.indexOf(b.ID))
+    .map((object) => {
+      const {ID, HOMBRES, MUJERES, NOM_COMUNA} = object;
+      return {ID, HOMBRES, MUJERES, NOM_COMUNA};
+    });
+  const stack = d3
+    .stack()
+    .keys(['HOMBRES', 'MUJERES'])
+  const series = stack(data);
+  const maxValue = d3.max(series, (s) => d3.max(s, (array) => array[1]));
+
+  // Dimensions
+  const width = 560;
+  const height = 400;
+
+  // Scales
+  const scaleX = d3.scaleBand()
+    .domain(data.map((d) => d.NOM_COMUNA))
+    .rangeRound([0, width])
+    .padding(0.3)
+  const scaleY = d3.scaleLinear()
+    .domain([0, maxValue])
+    .range([height, 0])
+  const scaleColor = d3.scaleOrdinal()
+    .domain(series.keys())
+    .range(['red', 'blue'])
+
+  // Create/Update graph
+  const graph = d3.select('#graph');
+  graph
+    .selectAll('g')
+    .data(series)
+    .join('g')
+      .attr('fill', (d) => scaleColor(d.key))
+      .selectAll('rect')
+      .data((d) => d, (d) => d.data.NOM_COMUNA)
+      .join((enter) =>
+        enter
+          .append('rect')
+          .attr('class', (d) => d.data.NOM_COMUNA.replace(/\s+/g, ''))
+          .attr('x', (d) => scaleX(d.data.NOM_COMUNA))
+          .attr('width', scaleX.bandwidth())
+          .attr('y', height)
+          .attr('height', 0)
+          .transition()
+          .duration(800)
+          .attr('y', (d) => scaleY(d[1]))
+          .attr('height', (d) => scaleY(d[0]) - scaleY(d[1]))
+          .selection(),
+        (update) =>
+        update
+          .transition()
+          .duration(800)
+          .attr('x', (d) => scaleX(d.data.NOM_COMUNA))
+          .attr('width', scaleX.bandwidth())
+          .attr('y', (d) => scaleY(d[1]))
+          .attr('height', (d) => scaleY(d[0]) - scaleY(d[1]))
+          .selection(),
+        (exit) =>
+        exit
+          .transition()
+          .duration(500)
+          .attr('y', height)
+          .attr('height', 0)
+          .remove()
+      )
+      .on('mouseenter', (_, d) => {
+        const total = parseInt(d.data.HOMBRES, 10) + parseInt(d.data.MUJERES, 10);
+        let amount = 0;
+        let detailType = '';
+        let color = '';
+        if (!d[0]) {
+          amount = parseInt(d.data.HOMBRES);
+          detailType = 'Hombres';
+          color = '#F35C5C';
+        } else {
+          amount = parseInt(d.data.MUJERES);
+          detailType = 'Mujeres';
+          color = '#3F77E8';
+        }
+        const proportion = Math.round(100 * ((amount * 100) / total)) / 100;
+        let offset = scaleX.bandwidth() + 10;
+        if (scaleX(d.data.NOM_COMUNA) > (width / 2)) {
+          offset = - 130;
+        }
+        const details = graph.append('g')
+          .attr('transform', `translate(${scaleX(d.data.NOM_COMUNA) + offset}, ${scaleY(d[1]) - 15})`)
+          .attr('class', 'detail')
+        details.append('rect')
+          .attr('width', 120)
+          .attr('height', 120)
+          .attr('rx', 20)
+          .attr('fill', color)
+          .attr('stroke', 'black')
+          .attr('opacity', 0.9)
+        details.append('text').text(detailType).attr('x', 25).attr('y', 30).attr('class', 'label');
+        details.append('text').text(`Total: ${amount}`).attr('x', 10).attr('y', 60).attr('class', 'label');
+        details.append('text').text(`${proportion}%`).attr('x', 30).attr('y', 95).attr('class', 'big-label');
+        graph.selectAll(`.${d.data.NOM_COMUNA.replace(/\s+/g, '')}`).attr('opacity', 0.8);
+        d3.select('#current-commune').text(`Comuna Actual: ${d.data.NOM_COMUNA}`);
+      })
+      .on('mouseleave', (_, d) => {
+        graph.selectAll('.detail').remove();
+        d3.select('#current-commune').text(`Comuna Actual:`);
+        graph.selectAll(`.${d.data.NOM_COMUNA.replace(/\s+/g, '')}`).attr('opacity', 1);
+      })
+
+  // Axis
+  const axisX = d3.axisBottom(scaleX);
+  if (data.length > 60) {
+    axisX.tickValues([]);
+  }
+  const axisY = d3.axisLeft(scaleY);
+  graph
+    .append('g')
+      .attr('transform', `translate(0, ${height})`)
+    .call(axisX)
+    .selectAll('text')
+      .attr('y', -4)
+      .attr('x', 9)
+      .attr('transform', 'rotate(90)')
+      .style('text-anchor', 'start')
+  graph
+    .append('g')
+      .attr('transform', `translate(0, 0)`)
+    .call(axisY)
+    .selectAll('line')
+      .attr('x1', width)
+      .attr('opacity', 0.1)
+      .attr('pointer-events', 'none')
 }
 
 // Info View
@@ -331,22 +520,17 @@ const info = (census) => {
 // Load Data
 const initialize = async () => {
   /* Columns: name, release_date, developer, publisher, platforms,
-    genres, price, positive, negative, steam_appid, short_description,
+    genres, steamspy_tags, price, positive, negative, steam_appid, short_description,
     header_image, minimum */
-  const games = await d3.csv('../data/steam.csv');
-  /*
-  const parseData = (object) => {
-    object.ID = parseInt(object.ID, 10);
-    return object;
-  }
-  census = census.map(parseData);
-  */
-  return games.filter((game) => parseInt(game.steam_appid, 10) <= 10);
+  games = await d3.csv('../data/steam.csv');
+  currentGames = games.slice();
+  /* Levels: genres -> dates -> tags -> games */
+  tree = await d3.json('../data/network.json');
 }
 
 // Initialize Visualization
-initialize().then((games) => {
-  gameNetwork(network.height, network.width, games);
+initialize().then(() => {
+  gameNetwork(network.height, network.width);
   // Search View
   // Comparison View
   // Recommendation
